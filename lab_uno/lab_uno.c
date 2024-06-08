e83644843d62a1fda4ffb6f2e73602598de57201
Artemii <angap4@gmail.com>
2024-06-08 19:22:08 +1000

    reformat

--- lab_uno/lab_uno.c
+++ lab_uno/lab_uno.c
@@ -3,43 +3,51 @@
 #include <xtimer.h>
 #include <board.h>
 
-#define sec (1000000) // 1000 milisec
+#define long_press 5
 
 // uint32_t debounce_timestamp = 0;
 struct tm cur_time;
 struct tm next_time;
 
-
-void alarm_func(void* arg){
+void alarm_func(void *arg)
+{
     (void)arg;
     puts("ALARM!");
 }
 
-void change_mode(void* arg){
+void change_mode(void *arg)
+{
     (void)arg;
 
-    if (gpio_read(BTN0_PIN) > 0){
+    // Активация таймера на длительное нажатие
+    // В случае недодержания активируется под-функция режима (else)
+    if (gpio_read(BTN0_PIN) > 0)
+    {
         // debounce_timestamp = xtimer_usec_from_ticks(xtimer_now());
         puts("a");
         rtc_get_time(&next_time);
-        next_time.tm_sec += 5;
+        next_time.tm_sec += long_press;
 
         rtc_set_alarm(&next_time, alarm_func, NULL);
     }
-    else{
+    else
+    {
         rtc_get_time(&cur_time);
-        if (cur_time.tm_min <= next_time.tm_min  && cur_time.tm_sec < next_time.tm_sec){
+        if (cur_time.tm_min <= next_time.tm_min && cur_time.tm_sec < next_time.tm_sec)
+        {
             puts("b");
             rtc_clear_alarm();
         }
     }
 }
 
-int main (void){
+int main(void)
+{
     gpio_t cur_LED = LED3_PIN;
     gpio_t btn_pin = BTN0_PIN;
-    gpio_init_int(btn_pin, GPIO_IN, GPIO_BOTH, change_mode,  NULL);
-    while(true){
+    gpio_init_int(btn_pin, GPIO_IN, GPIO_BOTH, change_mode, NULL);
+    while (true)
+    {
         // if (gpio_read(btn_pin)){
         //     gpio_clear(cur_LED);
         //     puts("button pressed");
@@ -49,5 +57,4 @@ int main (void){
         gpio_clear(cur_LED);
         xtimer_sleep(2);
     }
-
 }
